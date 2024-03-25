8043ef8ce8c760a414d55406f668c6c7ac938688
Mariano978 <marianofrank@hotmail.com>
2024-03-25 20:36:54 -0300

    250324-3

--- config/db.js
+++ config/db.js
@@ -1,5 +1,6 @@
 import { Sequelize } from "sequelize";
 import "dotenv/config";
+
 const db = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
@@ -12,20 +13,19 @@ const db = new Sequelize(
   }
 );
 
-db.sync()
-  .then(() => {
-    console.log("Base de datos conectada");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+try {
+  await db.authenticate();
+  console.log("Base de datos conectada correctamente ✅.");
 
-db.query("CREATE EXTENSION IF NOT EXISTS postgis;")
-  .then(() => {
-    console.log("Extensión postgis creada correctamente");
-  })
-  .catch((error) => {
-    console.error("Error al crear la extensión postgis:", error);
-  });
+  db.query("CREATE EXTENSION IF NOT EXISTS postgis;")
+    .then(() => {
+      console.log("Extensión postgis creada correctamente");
+    })
+    .catch((error) => {
+      console.error("Error al crear la extensión postgis:", error);
+    });
+} catch (error) {
+  console.error("Error al conectar la base de datos ❌:", error);
+}
 
 export default db;
