d6a4159fe69963d1c4d1d08b091fe99a6cd1bbe9
Mariano978 <marianofrank@hotmail.com>
2024-04-07 15:34:54 -0300

    070424-2

--- src/js/mapaResultado.js
+++ src/js/mapaResultado.js
@@ -4,7 +4,7 @@ import { OpenStreetMapProvider } from "leaflet-geosearch";
 window.addEventListener("DOMContentLoaded", () => {
   const urlParams = new URLSearchParams(window.location.search);
   const centro = JSON.parse(urlParams.get("coordenadas"));
-
+  const radio = JSON.parse(urlParams.get("radio")) * 1000;
   if (!centro) {
     return;
   }
@@ -19,5 +19,29 @@ window.addEventListener("DOMContentLoaded", () => {
       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   }).addTo(map);
 
-  const provider = new OpenStreetMapProvider();
+  L.circle(centro, radio, {
+    color: "#99CFFF",
+    fillColor: "#47a2f3",
+    opacity: 0.3,
+    fillOpacity: 0.3,
+  }).addTo(map);
+
+  //marcadores
+  const coordenadas = document.querySelectorAll("#coordenadas-meeti");
+
+  coordenadas.forEach((coordenadaElement) => {
+    const coordenada = JSON.parse(coordenadaElement.value).reverse();
+    const enlaceMeeti = coordenadaElement.parentElement.querySelector("a");
+    const enlace = document.createElement("A");
+    enlace.href = enlaceMeeti.href;
+    enlace.classList.add("enlace-mapa");
+    enlace.textContent = enlaceMeeti.textContent;
+    L.marker(coordenada, {
+      draggable: false,
+      autoPan: false,
+    })
+      .bindPopup(enlace)
+      .addTo(map)
+      .openPopup();
+  });
 });
